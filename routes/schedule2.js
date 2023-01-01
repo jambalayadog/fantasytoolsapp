b04b5ef9aa6363d58f7d76b88043790d68c40685
jambalaya <jambalayadog77@gmail.com>
2023-01-01 14:53:07 -0800

    schedule get fetches from github

--- routes/schedule2.js
+++ routes/schedule2.js
@@ -6,10 +6,12 @@ var fs = require('fs')
 const schedule_file = "files/01_NHLScheduleByTeam.json"
 //const schedule_url = "https://fantasyhockeycentral.com/fantasytoolsapp/files/01_NHLScheduleByTeam.json"
 const schedule_url = "https://fantasyhockeycentral.com/fantasytoolsapp/files/01_NHLScheduleByTeam.json"
+const schedule_url_2 = "https://raw.githubusercontent.com/jambalayadog/fantasytoolsapp/main/files/01_NHLScheduleByTeam.json"
 var schedule_data
 
 const teamStats_file = "files/02_NHLTeamStats.json"
 const teamStats_url = "https://fantasyhockeycentral.com/fantasytoolsapp/files/02_NHLTeamStats.json"
+const teamStats_url_2 = "https://raw.githubusercontent.com/jambalayadog/fantasytoolsapp/main/files/02_NHLTeamStats.json"
 
 var teamstats_data
 
@@ -60,6 +62,7 @@ router.get('/', function(req, res, next) {
 /*
 /* GET home page. */
 
+/*
 router.get('/', function(req, res, next) {
     fs.readFile(schedule_file, 'utf8', (err, data) => {
         if (err) {
@@ -86,6 +89,41 @@ router.get('/', function(req, res, next) {
     });
 });
 
+
+*/
+
+router.get('/', function(req, res, next) {
+
+    fetch(schedule_url_2, {
+        method: 'GET',
+        credentials: 'same-origin',
+        mode: 'cors',
+        headers: {
+            accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'http://159.203.66.92',
+            'Access-Control-Allow-Credentials': true,
+        },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        schedule_data = data
+        //console.log('1a ', schedule_data, typeof(schedule_data))
+        
+        fetch(teamStats_url_2)
+        .then((response) => response.json())
+        .then((data) => {
+            teamstats_data = data
+            //console.log('2a ', {teamstats_data})
+            res.render('schedule2', {schedule_data: schedule_data, teamstats_data: teamstats_data});
+        }).catch((err) => {
+            console.log(err);    
+        })
+    }).catch((err) => {
+        console.log(schedule_url, err);
+    });
+
+});
 
 
 
