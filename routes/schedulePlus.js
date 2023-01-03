6722037574567979f5e318d6c9b9ab004cfa71a5
jambalaya <jwatt4000@gmail.com>
2023-01-03 14:59:50 -0800

    scheduleplus: date set to noon

--- routes/schedulePlus.js
+++ routes/schedulePlus.js
@@ -15,51 +15,6 @@ const teamStats_url_2 = "https://raw.githubusercontent.com/jambalayadog/fantasyt
 
 var teamstats_data
 
-/*
-
-        headers: {
-            accept: 'application/json',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': 'http://159.203.66.92',
-            'Access-Control-Allow-Credentials': true,
-        },
-
-*/
-/*
-router.get('/', function(req, res, next) {
-
-    fetch(schedule_url_2, {
-        method: 'GET',
-        credentials: 'include',
-        mode: 'cors',
-        headers: {
-            accept: 'application/json',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Credentials': true,
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': '*'
-        },
-    })
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error(`HTTP error: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then((data) => {
-        schedule_data = data
-        //console.log('1a ', schedule_data, typeof(schedule_data))
-        res.render('./game/game', { title: 'Hey', message: 'Hello there!' });
-    }).catch((err) => {
-        //console.log(res)
-        console.log(schedule_url, err, err.message);
-        res.render('teams')
-    });
-
-});*/
-
-
-
 router.get('/', function(req, res, next) {
 
     fetch(schedule_url_2, {
@@ -83,7 +38,7 @@ router.get('/', function(req, res, next) {
         .then((data) => {
             teamstats_data = data
             //console.log('2a ', {teamstats_data})
-            res.render('schedule2', {schedule_data: schedule_data, teamstats_data: teamstats_data});
+            res.render('schedulePlus', {schedule_data: schedule_data, teamstats_data: teamstats_data});
         }).catch((err) => {
             console.log(err);    
         })
@@ -92,61 +47,6 @@ router.get('/', function(req, res, next) {
     });
 
 });
-
-/*
-
-router.get('/', function(req, res, next) {
-    var req = new XMLHttpRequest();
-    req.responseType = 'json';
-    req.open('GET', schedule_url, true);
-    req.onload  = function() {
-        var jsonResponse = req.response;
-        // do something with jsonResponse
-        schedule_data = req.response
-        res.render('teams');
-    };
-    req.send(null);
-});
-
-console.log('Fetch test here ---> ')
-
-*/
-
-/* GET home page. 
-router.get('/', function(req, res, next) {
-    res.render('schedule', { title: 'Express2' });
-});*/
-
-
-/*
-/* GET home page. */
-/*
-router.get('/', function(req, res, next) {
-    fs.readFile(schedule_file, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        //console.log(data);
-        schedule_data = data
-        //res.render('schedule', {schedule_data: schedule_data});
-        fs.readFile(teamStats_file, 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            //console.log(data);
-            teamstats_data = data
-            var combined_data = JSON.stringify({schedule_data: schedule_data, teamstats_data: teamstats_data})
-            console.log('1 ', (schedule_data).substring(0,50))
-            console.log('2 ', (teamstats_data).substring(0,55))
-            console.log('3b ', (combined_data).substring(0,60))
-            res.render('players', {schedule_data: schedule_data, teamstats_data: teamstats_data});
-            //res.render('schedule2', JSON.stringify({schedule_data: schedule_data, teamstats_data: teamstats_data}));
-        });
-    });
-});*/
-
 
 
 
