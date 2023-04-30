f8d09b8f655207c639a0037a1c568ddfa537e8db
jambalaya <jwatt4000@gmail.com>
2023-04-30 08:36:52 -0700

    logging

--- routes/gameDesign/conceptDiscovery.js
+++ routes/gameDesign/conceptDiscovery.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs')
 
-
+console.log('conceptDiscovery.js ---> ')
 
 router.get('/', function(req, res, next) {
     res.render('./gameDesign/conceptDiscovery', { title: 'Concept Discovery', message: 'Hello there!' });
